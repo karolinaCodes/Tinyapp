42940da039d24549d9044e776a4bf905b499c8fc
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-08 13:43:46 -0500

    add new endpoint /urls and pass templateVars object with the urlDatabse in the the url index page

--- express_server.js
+++ express_server.js
@@ -9,16 +9,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
